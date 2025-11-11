3ef413ee52885eaa51fb625120e50e6df6ecd3e0
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-11 14:29:56 +0330

    Added Logout Page

---
+++ src/app/(protected)/logout/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { userActions } from "@/src/lib/module/user";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function LogoutPage() {
+    const router = useRouter();
+    useEffect(() => {
+        userActions.logout()
+            .then((res) => {
+                console.log(res.data);
+                router.refresh();
+                router.replace('/');
+            }).catch(error => {
+                console.error(error);
+            })
+
+    }, [])
+    return <></>
+}
