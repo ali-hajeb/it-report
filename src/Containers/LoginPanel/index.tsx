fe9fd1df7d1418b8fbe734ae6e88cdd59ec3c6dc
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-11 14:35:30 +0330

    Fixed an issue

--- src/Containers/LoginPanel/index.tsx
+++ src/Containers/LoginPanel/index.tsx
@@ -50,7 +50,7 @@ export default function LoginPanel({}) {
         <Container w={550}>
             <Center>
                 <Paper py={'md'} px={'lg'} bg={'white'} shadow='xl' radius={'md'}>
-                    <Image src={Logo} style={{margin: '0 auto'}} alt='logo' width={256} height={256}/>
+                    <Image loading="eager" src={Logo} style={{margin: '0 auto'}} alt='logo' width={256} height={256}/>
                     <Title order={1} ta={'center'} c={'violet.7'} mb={10} fz={'h2'}>
                         سامانه گزارش و ثبت مستندات واحد IT
                     </Title>
@@ -75,7 +75,7 @@ export default function LoginPanel({}) {
                                     ورود
                                 </Button>
                                 <Button variant='outline' onClick={async () => {
-                                    await axiosInstance.post('/auth/register', { firstName: 'admin', lastName: 'admin', username: 'admin', password: 'admin'})
+                                    await axiosInstance.post('/auth/register', { firstName: 'admin', lastName: 'admin', username: 'admin', password: 'admin', location: '690eeb31852c7d9936ae31f8'})
                                 }}>test</Button>
                             </Stack>
                         </Form>
