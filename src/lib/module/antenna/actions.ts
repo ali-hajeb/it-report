43090a89a4db2579486eb877a1599e513409e417
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-11 14:38:07 +0330

    Fixed Spelling

--- src/lib/module/antenna/actions.ts
+++ src/lib/module/antenna/actions.ts
@@ -3,42 +3,42 @@ import type IAnthena from "./antenna.types.js";
 import type { INewAntenna, INewAntennaLink, IAntennaLink } from "./antenna.types.js";
 
 export async function createAntenna(data: INewAntenna) {
-    return axiosInstance.post('/anthena', data);
+    return axiosInstance.post('/antenna', data);
 }
 
 export async function updateAntenna({ _id, ...updatedData }: IAnthena) {
-    return axiosInstance.patch('/anthena', {_id, ...updatedData});
+    return axiosInstance.patch('/antenna', {_id, ...updatedData});
 }
 
 export async function deleteAntenna(id: string) {
-    return axiosInstance.delete(`/anthena/${id}`);
+    return axiosInstance.delete(`/antenna/${id}`);
 }
 
-export async function getAntennas(params: Record<string, string>) {
-    return axiosInstance.get('/anthena', { params })
+export async function getAntennas(params: Record<string, string | undefined>) {
+    return axiosInstance.get('/antenna', { params })
 }
 
 export async function getAntennaById(id: string) {
-    return axiosInstance.get(`/anthena/${id}`);
+    return axiosInstance.get(`/antenna/${id}`);
 }
 
 export async function createAntennaLink(data: INewAntennaLink) {
-    return axiosInstance.post('/anthena/link', data);
+    return axiosInstance.post('/antenna/link', data);
 }
 
 export async function updateAntennaLink({ _id, ...updatedData }: IAntennaLink) {
-    return axiosInstance.patch('/anthena/link', {_id, ...updatedData});
+    return axiosInstance.patch('/antenna/link', {_id, ...updatedData});
 }
 
 export async function deleteAntennaLink(id: string) {
-    return axiosInstance.delete(`/anthena/link/${id}`);
+    return axiosInstance.delete(`/antenna/link/${id}`);
 }
 
 export async function getAntennasLink(params: Record<string, string>) {
-    return axiosInstance.get('/anthena/link', { params })
+    return axiosInstance.get('/antenna/link', { params })
 }
 
 export async function getAntennaLinkById(id: string) {
-    return axiosInstance.get(`/anthena/link/${id}`);
+    return axiosInstance.get(`/antenna/link/${id}`);
 }
 
