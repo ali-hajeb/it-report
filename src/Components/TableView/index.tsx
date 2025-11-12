187ccefa7e6c36e130ae6f762e7d5ba6499fcf2f
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-12 13:32:28 +0330

    Changed component's exports

--- src/Components/TableView/index.tsx
+++ src/Components/TableView/index.tsx
@@ -1,7 +1,9 @@
 import { Box } from "@mantine/core"
 import Modal from './Modal';
 import TopBar from "./TopBar";
-import TableContainer from "./Table";
+import TableContainer from "./TableContainer";
+import { renderFormFromSchema } from './utils';
+import { IBaseModelObject, ITableViewField, SelectOption } from './types';
 
 export interface TableViewProps extends React.PropsWithChildren {
 }
@@ -17,3 +19,9 @@ TableView.TopBar = TopBar;
 TableView.TableContainer = TableContainer;
 
 export default TableView;
+export type {
+    ITableViewField,
+    IBaseModelObject,
+    SelectOption,
+};
+export { renderFormFromSchema };
