6b787acf8313289ab6b04db1327485c2db8db4a7
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-25 15:20:50 +0330

    feat: Added view mode

--- src/Components/TableView/Modal.tsx
+++ src/Components/TableView/Modal.tsx
@@ -1,21 +1,29 @@
-import { Modal } from '@mantine/core'
+import { toFarsiNumber } from '@/src/utils/number';
+import { Modal, Table } from '@mantine/core'
 import { PropsWithChildren } from 'react';
+import { IBaseModelObject, ITableViewField } from './types';
 
-export interface TableViewModalProps extends PropsWithChildren {
+export interface TableViewModalProps<T extends IBaseModelObject> extends PropsWithChildren {
+    fields?: ITableViewField<T>[];
+    customFieldValue?: (data: T, field: keyof T) => string | React.ReactElement | null
     opened: boolean;
     close: () => void;
     closeHandler: () => void;
     editMode: string | null;
     title: string;
+    viewMode?: T | null;
 }
-export default function TableViewModal({
+export default function TableViewModal<T extends IBaseModelObject>({
     opened,
     close,
     closeHandler,
     editMode,
     title,
+    viewMode,
+    fields,
+    customFieldValue,
     children
-}: TableViewModalProps) {
+}: TableViewModalProps<T>) {
     const modalOnCloseHandler = () => {
         closeHandler();
         // close();
@@ -23,7 +31,28 @@ export default function TableViewModal({
     return <Modal
         opened={opened} 
         onClose={modalOnCloseHandler}
-        title={editMode ? `ویرایش ${title}` : `افزودن ${title}`}>
-        {children}
+        title={viewMode ? 'نمایش' : editMode ? `ویرایش ${title}` : `افزودن ${title}`}>
+        { viewMode && fields && customFieldValue ? <Table variant='vertical'>
+            <Table.Tbody>
+                {
+                    fields.map(field =>  <Table.Tr key={field.key.toString()} >
+                        <Table.Th 
+                            fz={'xs'}
+                            key={`${field.key.toString()}-${field.title}`}
+                            id={field.key.toString()}
+                            ta={'center'}
+                            title={field.alt}>
+                            {field.title}
+                        </Table.Th>
+                        <Table.Td 
+                            fz={'xs'}
+                            ta={'center'}
+                            title={field.alt}>
+                            {customFieldValue(viewMode, field.key)}
+                        </Table.Td>
+                    </Table.Tr>)
+                }
+            </Table.Tbody>
+        </Table> : children}
     </Modal>
 }
