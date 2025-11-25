1cf18919883d3360f58794d3df51b27dc5586492
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-25 15:22:03 +0330

    Added Color list

---
+++ src/Constants/colors.ts
@@ -0,0 +1,2 @@
+import { DefaultMantineColor } from '@mantine/core';
+export const COLORS: DefaultMantineColor[] = ['dark', 'cyan', 'blue', 'green', 'yellow', 'lime', 'orange', 'indigo', 'grape', 'gray', 'violet', 'red', 'pink', 'teal'];
