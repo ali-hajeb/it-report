d7be03c7b34a4070f832dfd4f5eef6bfac5a6aa0
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-25 15:20:13 +0330

    Added custom field support

---
+++ src/Components/Server/utils.tsx
@@ -0,0 +1,79 @@
+import { Badge } from '@mantine/core';
+import { IServerPopulated } from "@/src/lib/module/common/types";
+import { ILocation } from "@/src/lib/module/location";
+import IServer from "@/src/lib/module/server/server.types";
+import { toFarsiNumber } from "@/src/utils/number";
+import moment from "jalali-moment";
+
+export function getCustomFieldValue(data: IServerPopulated, field: keyof IServer) {
+    if (!data[field]) {
+        return null;
+    }
+
+    switch (field) {
+        case 'location': 
+            return (data[field] as ILocation).name;
+        case 'lastUpdateDate':
+            return toFarsiNumber(moment(data[field]).format('jyyyy/jmm/jdd'));
+        case 'launchDate':
+            return toFarsiNumber(moment(data[field]).format('jyyyy/jmm/jdd'));
+        case 'activeServices': 
+            return (data[field] as string[]).join(', ');
+        case 'importantSoftware': 
+            return (data[field] as string[]).join(', ');
+        case 'openPorts': 
+            return (data[field] as number[]).join(', ');
+        case 'backupStatus': {
+            let state = '';
+            let text = '';
+            switch (data[field]) {
+                case "Active": {
+                    state = 'green';
+                    text = 'فعال';
+                    console.log('status', data[field]);
+                    break;
+                }
+                case "Inactive": {
+                    state = 'red';
+                    text = 'غیرفعال';
+                    break;
+                }
+                default: {
+                    state = 'gray';
+                    text = 'نامشخص';
+                }
+            }
+            return (<Badge variant="light" color={state}>{text}</Badge>)
+        }
+        case 'currentStatus': {
+            let state = '';
+            let text = '';
+            switch (data[field]) {
+                case "Active": {
+                    state = 'green';
+                    text = 'روشن';
+                    console.log('status', data[field]);
+                    break;
+                }
+                case "Down": {
+                    state = 'red';
+                    text = 'خاموش';
+                    break;
+                }
+                case "Reserved": {
+                    state = 'yellow';
+                    text = 'رزرو';
+                    break;
+                }
+                default: {
+                    state = 'gray';
+                    text = 'نامشخص';
+                }
+            }
+            return (<Badge variant="light" color={state}>{text}</Badge>)
+        }
+        default: {
+            return data[field]?.toLocaleString();
+        }
+    }
+}
