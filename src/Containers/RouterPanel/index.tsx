1d700f44209eec8072dffb386cdb7c542a77eb28
Ali Hajeb <ali.hajeb@hotmail.com>
2025-11-26 21:53:21 +0330

    Completed Router module

--- src/Containers/RouterPanel/index.tsx
+++ src/Containers/RouterPanel/index.tsx
@@ -3,8 +3,10 @@ import { useContext, useState } from "react"
 import { IconRouter, IconCloudDataConnection, IconSettingsCog, IconArrowRightToArc, IconArrowBackUp, IconDatabaseImport } from '@tabler/icons-react';
 import { Box, Tabs, Title } from '@mantine/core';
 import UserContext from "@/src/Contexts/UserContext";
-import { IRouterPopulated } from "@/src/lib/module/common/types";
+import { IRouterBackupPopulated, IRouterInterfacePopulated, IRouterPopulated } from "@/src/lib/module/common/types";
 import RouterList from "@/src/Components/Router/RouterList";
+import RouterInterface from "@/src/Components/Router/RouterInterface";
+import RouterBackup from "@/src/Components/Router/Routerbackup";
 
 
 export interface RouterPannelProps {
@@ -19,6 +21,8 @@ export default function RouterPanel({
     const userContext = useContext(UserContext);
     const [activeTab, setActiveTab] = useState<string | null>('router-list');
     const [routers, setRouters] = useState<IRouterPopulated[]>([]);
+    const [routerInterfaces, setRouterInterfaces] = useState<IRouterInterfacePopulated[]>([]);
+    const [routerBackups, setRouterBackups] = useState<IRouterBackupPopulated[]>([]);
     // const [routerLinks, setRouterLinks] = useState<IRouterLinkPopulated[]>([]);
     // const [maintenanceReports, setMaintenanceReports] = useState<IMaintenanceReportPopulated[]>([]);
 
@@ -29,7 +33,7 @@ export default function RouterPanel({
                 <Tabs.List>
                     <Tabs.Tab value="router-list" leftSection={<IconRouter size={16} />}>فهرست روتر</Tabs.Tab>
                     <Tabs.Tab value="router-interface" leftSection={<IconArrowRightToArc size={16} />}>اینترفیس‌ها</Tabs.Tab>
-                    <Tabs.Tab value="router-backup" leftSection={<IconDatabaseImport size={16} />}>اطلاعات بکاپ</Tabs.Tab>
+                    <Tabs.Tab value="router-backup" leftSection={<IconDatabaseImport size={16} />}>اطلاعات پشتیبان‌گیری</Tabs.Tab>
                 </Tabs.List>
 
                 <Box px='xs'>
@@ -39,10 +43,14 @@ export default function RouterPanel({
                             setRouters={setRouters} />
                     </Tabs.Panel>
                     <Tabs.Panel value="router-interface">
-                        <></>
+                        <RouterInterface location={userContext?.role === 'ADMIN' ? userContext.location._id : location}
+                            routerInterfaces={routerInterfaces}
+                            setRouterInterfaces={setRouterInterfaces} />
                     </Tabs.Panel>
                     <Tabs.Panel value="router-backup">
-                        <></>
+                        <RouterBackup location={userContext?.role === 'ADMIN' ? userContext.location._id : location}
+                            routerBackups={routerBackups}
+                            setRouterBackups={setRouterBackups} />
                     </Tabs.Panel>
                 </Box>
             </Tabs>
