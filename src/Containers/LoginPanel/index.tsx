9b77e45614db01f2ccc81c1faec6a7947392ff52
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-07 13:54:43 +0330

    Added Copyright text

--- src/Containers/LoginPanel/index.tsx
+++ src/Containers/LoginPanel/index.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Box, Button, Center, Container, Paper, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import { Box, Button, Center, Container, Paper, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
 import { Form, useForm } from "@mantine/form";
-import axiosInstance from "@/src/config/axios";
 import Logo from '@/public/logo-violet.png';
 import { userActions } from "@/src/lib/module/user";
+import { ISiteConfig, siteConfigActions } from "@/src/lib/module/config";
 
 export type LoginForm = {
     username: string;
@@ -19,6 +19,7 @@ export type LoginForm = {
 
 export default function LoginPanel({}) {
     const [isLoading, setLoading] = useState(false);
+    const [copyright, setCopyright] = useState<string[]>([]);
     const router = useRouter();
 
     const loginForm = useForm<LoginForm>({
@@ -28,6 +29,15 @@ export default function LoginPanel({}) {
             password: '',
         }
     })
+
+    useEffect(() => {
+        siteConfigActions.getSiteConfig()
+            .then((res) => {
+                setCopyright((res.data.config as ISiteConfig).copyright || []);
+            }).catch(err => {
+                console.error(err);
+            })
+    }, []);
 
     const loginOnClickHandler = ({ username, password }: LoginForm) => {
         setLoading(true);
@@ -49,38 +59,43 @@ export default function LoginPanel({}) {
     return (
         <Container w={550}>
             <Center>
-                <Paper py={'md'} px={'lg'} bg={'white'} shadow='xl' radius={'md'}>
-                    <Image loading="eager" src={Logo} style={{margin: '0 auto'}} alt='logo' width={256} height={256}/>
-                    <Title order={1} ta={'center'} c={'violet.7'} mb={10} fz={'h2'}>
-                        سامانه گزارش و ثبت مستندات واحد IT
-                    </Title>
-                    <Title order={2} ta={'center'} c={'gray.6'} fz={'h4'}>
-                        دانشکده علوم پزشکی بهبهان
-                    </Title>
+                <Box>
+                    <Paper py={'md'} px={'lg'} bg={'white'} shadow='xl' radius={'md'}>
+                        <Image loading="eager" src={Logo} style={{margin: '0 auto'}} alt='logo' width={256} height={256}/>
+                        <Title order={1} ta={'center'} c={'violet.7'} mb={10} fz={'h2'}>
+                            سامانه گزارش و ثبت مستندات واحد IT
+                        </Title>
+                        <Title order={2} ta={'center'} c={'gray.6'} fz={'h4'}>
+                            دانشکده علوم پزشکی بهبهان
+                        </Title>
 
-                    <Box>
-                        <Form form={loginForm} onSubmit={loginOnClickHandler}>
-                            <Stack gap={'md'} mt={'md'}>
-                                <TextInput
-                                    label="نام کاربری"
-                                    key={loginForm.key('username')}
-                                    {...loginForm.getInputProps('username')}
-                                />
-                                <PasswordInput
-                                    label="گذرواژه"
-                                    key={loginForm.key('password')}
-                                    {...loginForm.getInputProps('password')}
-                                />
-                                <Button loading={isLoading} type="submit" fullWidth>
-                                    ورود
-                                </Button>
-                                {/* <Button variant='outline' onClick={async () => { */}
-                                {/*     await axiosInstance.post('/auth/register', { firstName: 'admin', lastName: 'admin', username: 'admin', password: 'admin', location: '690eeb31852c7d9936ae31f8'}) */}
-                                {/* }}>test</Button> */}
-                            </Stack>
-                        </Form>
-                    </Box>
-                </Paper>
+                        <Box>
+                            <Form form={loginForm} onSubmit={loginOnClickHandler}>
+                                <Stack gap={'md'} mt={'md'}>
+                                    <TextInput
+                                        label="نام کاربری"
+                                        key={loginForm.key('username')}
+                                        {...loginForm.getInputProps('username')}
+                                    />
+                                    <PasswordInput
+                                        label="گذرواژه"
+                                        key={loginForm.key('password')}
+                                        {...loginForm.getInputProps('password')}
+                                    />
+                                    <Button loading={isLoading} type="submit" fullWidth>
+                                        ورود
+                                    </Button>
+                                    {/* <Button variant='outline' onClick={async () => { */}
+                                    {/*     await axiosInstance.post('/auth/register', { firstName: 'admin', lastName: 'admin', username: 'admin', password: 'admin', location: '690eeb31852c7d9936ae31f8'}) */}
+                                    {/* }}>test</Button> */}
+                                </Stack>
+                            </Form>
+                        </Box>
+                    </Paper>
+                    <Stack align="center" gap={0} mt={'md'}>
+                        { copyright.map((t,i) => <Text key={i} fz='xs' c={'gray.0'} dir="auto">{t}</Text>) }
+                    </Stack>
+                </Box>
             </Center>
         </Container>
     );
