60647063d20ebe9f4feea21754fbcc1c094da652
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-07 13:51:37 +0330

    Removed dark colors

--- src/Constants/colors.ts
+++ src/Constants/colors.ts
@@ -1,2 +1,3 @@
 import { DefaultMantineColor } from '@mantine/core';
-export const COLORS: DefaultMantineColor[] = ['dark', 'cyan', 'blue', 'green', 'yellow', 'lime', 'orange', 'indigo', 'grape', 'gray', 'violet', 'red', 'pink', 'teal'];
+// export const COLORS: DefaultMantineColor[] = ['dark', 'cyan', 'blue', 'green', 'yellow', 'lime', 'orange', 'indigo', 'grape', 'gray', 'violet', 'red', 'pink', 'teal'];
+export const COLORS: DefaultMantineColor[] = ['cyan', 'blue', 'green', 'yellow', 'lime', 'orange', 'indigo', 'grape', 'violet', 'red', 'pink', 'teal'];
