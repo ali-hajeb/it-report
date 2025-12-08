0a256b3c4c57f1def1a165bc2ca7c8f3d2d01e15
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-08 14:27:55 +0330

    Fixed a bug

--- src/app/api/dashboard/config/route.ts
+++ src/app/api/dashboard/config/route.ts
@@ -1,10 +1,17 @@
 import Config from "@/src/lib/module/config";
+import connectDB from '@/src/lib/database/dbConnect';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
     try {
+        await connectDB();
         const config = await Config.findOne();
+        // if (config.length < 1) {
+        //     return NextResponse.json({ code: 404 }, { status: 404});
+        // }
         return NextResponse.json({ code: 200, config }, { status: 200});
     } catch (error) {
+        console.error(error);
+        return NextResponse.json({ code: 400, data: error }, { status: 400});
     }
 }
