afc9046f3fb6a2404dc69106a49bb7ee6ab39010
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-15 09:08:24 +0330

    Updated

--- src/Containers/LocationPanel/SingleLocation.tsx
+++ src/Containers/LocationPanel/SingleLocation.tsx
@@ -41,7 +41,7 @@ export default function SingleLocationPanel() {
     }, [])
 
     const btnBackOnClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        router.back();
+        router.replace('location');
     }
 
     return <>
