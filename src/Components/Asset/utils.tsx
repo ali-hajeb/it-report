a8d35b7965473bf7d2f6f7853f3f59d5ba4ffcbc
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-21 10:49:05 +0330

    Updated style

--- src/Components/Asset/utils.tsx
+++ src/Components/Asset/utils.tsx
@@ -1,15 +1,47 @@
-import { IAssetPopulated } from "@/src/lib/module/common/types";
+import { DeviceStatus, IAssetPopulated } from "@/src/lib/module/common/types";
 import { ILocation } from "@/src/lib/module/location";
+import { Badge } from "@mantine/core";
 
 export function getCustomFieldValue(data: IAssetPopulated, field: keyof IAssetPopulated) {
-    if (!data[field]) {
-        return null;
-    }
-
     switch (field) {
         case 'location': 
             return (data[field] as ILocation).name;
         default: {
+            if (field.toLowerCase().includes('status')) {
+                let state = '';
+                let text = '';
+                switch (data[field] as DeviceStatus) {
+                    case "عالی": {
+                        state = 'gold';
+                        text = data[field];
+                        break;
+                    }
+                    case "خوب": {
+                        state = 'silver';
+                        text = data[field];
+                        break;
+                    }
+                    case "متوسط": {
+                        state = 'bronze';
+                        text = data[field];
+                        break;
+                    }
+                    case "ضعیف": {
+                        state = 'red';
+                        text = data[field];
+                        break;
+                    }
+                    default: {
+                        state = 'gray';
+                        text = 'نامشخص';
+                    }
+                }
+                return <Badge 
+                    variant="light" 
+                    color={state}>
+                    {text}
+                </Badge>
+            }
             return data[field]?.toLocaleString();
         }
     }
